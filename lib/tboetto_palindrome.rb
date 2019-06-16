cb14c668e313480ca959dad5a08c120e8a0e125d
Tony Boetto <tonyboetto@Tonys-MacBook-Pro.local>
2019-06-16 11:20:20 -0500

    Finish work & refactored palindrome method

--- lib/tboetto_palindrome.rb
+++ lib/tboetto_palindrome.rb
@@ -1,6 +1,16 @@
 require "tboetto_palindrome/version"
 
-module TboettoPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# Modify the String class to check for palindromes
+class String
+  # Returns true for palindrome, otherwise false.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  # Processes string/integer to be compared
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
 end
